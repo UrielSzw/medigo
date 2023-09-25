592e380ff0f8d7fc6d282f65e49baf7a5e838820
Federico Peirano <fedepr2345@gmail.com>
2023-09-24 22:46:19 -0300

    Nueva route: RegisterDoctor

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Login, Register} from '../screens';
+import {Login, Register, RegisterDoctor} from '../screens';
 import {PATHS} from './paths';
 
 export const Routes = () => {
@@ -16,6 +16,7 @@ export const Routes = () => {
         initialRouteName={PATHS.LOGIN}>
         <Stack.Screen name={PATHS.REGISTER} component={Register} />
         <Stack.Screen name={PATHS.LOGIN} component={Login} />
+        <Stack.Screen name={PATHS.REGISTERDOCTOR} component={RegisterDoctor} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,2 +1,4 @@
 export * from './Common/Register/Register.screen';
 export * from './Common/Login/Login.screen';
+export * from './Doctor/RegisterDoc/RegisterDoc.screen';
+
